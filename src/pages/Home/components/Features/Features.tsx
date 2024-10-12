1c720713a06f2f37eb2a1da94a13c2896c8dc4ec
Osita Ezeigbo <osiclin@yahoo.com>
2024-10-12 21:49:10 +0100

    Added arrow assets

--- src/pages/Home/components/Features/Features.tsx
+++ src/pages/Home/components/Features/Features.tsx
@@ -2,6 +2,7 @@ import invest from "assets/images/invest.svg"
 import remember from "assets/images/remember.svg"
 import best from "assets/images/best.svg"
 import goals from "assets/images/goals.svg"
+import arrowRight from "assets/icons/arrow-right.svg"
 import { Link } from "react-router-dom"
 
 export const Features = () => {
@@ -11,7 +12,10 @@ export const Features = () => {
                 <div className="max-w-[376px] text-center md:text-left">
                     <h3 className="text-[20px] sm:text-[32px] text-heading-32 font-semibold mb-[12px]">Invest your money in dollars</h3>
                     <p className="text-base text-body-16 font-medium">By holding your investments in a stable currency, your money grows more over time and retains its value better.</p>
-                    <Link to="/start-investing" className="hidden md:inline mt-[24px]">Start Investing</Link>
+                    <Link to="/start-investing" className="hidden md:inline-block mt-[20px] text-teal-1 font-medium">
+                    Start Investing
+                    <img src={arrowRight} width={10} height={12} alt="arrow" className="inline"/>
+                    </Link>
                 </div>
                 <div className="order-first md:order-none">
                     <img src={invest} width={480} height={460} alt="invest" />
@@ -24,14 +28,20 @@ export const Features = () => {
                 <div className="max-w-[376px] text-center md:text-left">
                     <h3 className="text-[20px] sm:text-[32px] text-heading-32 font-semibold mb-[12px]">Choose what's best for you</h3>
                     <p className="text-base text-body-16 font-medium">Unlike other platforms, Rise lets you pick between stocks, US real estate and fixed income, according to your risk appetite. That way you can spread your risk and tap into different investments all in one place.</p>
-                    <Link to="/start-investing" className="hidden md:inline mt-[24px]">Start Investing</Link>
+                    <Link to="/start-investing" className="hidden md:inline-block mt-[20px] text-teal-1 font-medium">
+                    Start Investing
+                    <img src={arrowRight} width={10} height={12} alt="arrow" className="inline"/>
+                    </Link>
                 </div>
             </div>
             <div className="flex flex-col md:flex-row items-center justify-between">
                 <div className="max-w-[376px] text-center md:text-left">
                     <h3 className="text-[20px] sm:text-[32px] text-heading-32 font-semibold mb-[12px]">Set goals and reach them</h3>
                     <p className="text-base text-body-16 font-medium">You can invest towards a goal on Rise--retirement, higher education, save for your home or travel budgets. Or create a goal of your own and invest periodically to achieve them.</p>
-                    <Link to="/start-investing" className="hidden md:inline mt-[24px]">Start Investing</Link>
+                    <Link to="/start-investing" className="hidden md:inline-block mt-[20px] text-teal-1 font-medium">
+                    Start Investing
+                    <img src={arrowRight} width={10} height={12} alt="arrow" className="inline"/>
+                    </Link>
                 </div>
                 <div className="order-first md:order-none">
                     <img src={goals} width={480} height={460} alt="goals" />
@@ -44,7 +54,10 @@ export const Features = () => {
                 <div className="max-w-[376px] text-center md:text-left">
                     <h3 className="text-[20px] sm:text-[32px] text-heading-32 font-semibold mb-[12px]">We remember so you dont have to</h3>
                     <p className="text-base text-body-16 font-medium">Our Auto-invest feature makes it easy to stay consistent, even when you forget. Set a funding amount, frequency and payment method and Rise will automatically fund your investment, on schedule.</p>
-                    <Link to="/start-investing" className="hidden md:inline mt-[24px]">Start Investing</Link>
+                    <Link to="/start-investing" className="hidden md:inline-block mt-[20px] text-teal-1 font-medium">
+                    Start Investing
+                    <img src={arrowRight} width={10} height={12} alt="arrow" className="inline"/>
+                    </Link>
                 </div>
             </div>
         </div>
